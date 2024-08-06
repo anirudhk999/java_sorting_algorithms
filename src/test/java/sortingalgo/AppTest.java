3066ad71f5ac947ec2b8a7f8c026465c99686bf7
Kurnool Anirudh <Kurnool.Anirudh@LTTS.com>
2024-08-06 11:39:53 +0530

    jenkinsfile updated

--- src/test/java/sortingalgo/AppTest.java
+++ src/test/java/sortingalgo/AppTest.java
@@ -76,7 +76,7 @@ public class AppTest
     public void testQuickSort() {
         int[] arr = {5, 3, 8, 4, 2};
         int[] expectedAsc = {2, 3, 4, 5, 8};
-        int[] expectedDesc = {8, 5, 4, 3, 2};
+        int[] expectedDesc = {8, 5, 4, 2, 2};
  
         App.quickSort(arr, false);
         assertArrayEquals(expectedAsc, arr);
